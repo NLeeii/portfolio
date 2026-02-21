9b456a093b2f0f3a65cdf84cb83e31d8b7cb0745
NLeeii <niniabc920405@gmail.com>
2026-02-21 20:44:39 +0800

    feat: 新增共用 Button 組件，支援動態顏色、文字變更與外部連結

---
+++ src/components/common/Button.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+type ButtonProps = {
+  text: string;
+  color: 'white' | 'yellow';
+  icon?: string;
+  href: string;
+  target?: boolean;
+}
+
+defineProps<ButtonProps>()
+
+</script>
+<template>
+    <a :class="['btn', `btn_${color}`]" :href="href" :target="target ? '_blank' : undefined" :rel="target ? 'noopener noreferrer' : undefined">{{ text }}</a>
+</template>
+<style lang="scss" scoped>
+  .btn {
+    padding: 8px 24px;
+    @include body1;
+    border-radius: $radius_full;
+    transition: all .3s;
+  }
+  .btn_white {
+    background-color: $white;
+    border: 1px solid $gray;
+    &:hover {
+      background-color: $accent;
+      border: 1px solid $accent;
+    }
+  }
+  .btn_yellow {
+    border: 1px solid $white;
+    background-color: $accent;
+    &:hover {
+      background-color: $white;
+      border: 1px solid $white;
+    }
+  }
+</style>
