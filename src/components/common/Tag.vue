949afbd91fbf91d819c318b60c7627d4bce19ec0
NLeeii <niniabc920405@gmail.com>
2026-02-21 22:02:15 +0800

    feat: 新增共用 Tag 組件(用於顯示如技能、屬性等膠囊狀標籤)

---
+++ src/components/common/Tag.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+type TagProps = {
+  text: string;
+}
+
+defineProps<TagProps>()
+
+</script>
+<template>
+  <div class="tag">
+     <span>{{ text }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tag {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4px 14px;
+  border-radius: $radius_full;
+  border: 1px solid $gray_dark;
+  @include body3;
+  color: $text;
+}
+</style>
